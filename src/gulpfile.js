191fdfdc4975935395fc357b66a114e1f2c41de9
stefanboz <stefanbozic@gmail.com>
2017-10-31 21:02:58 +0100

    Added other project files

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var gulpBabel = require('gulp-babel');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('es6', function() {
     gulp.src('js/*.js')
@@ -7,6 +8,14 @@ gulp.task('es6', function() {
         .pipe(gulp.dest('jsoutput'));
 });
 
+gulp.task('styles', function() {
+    gulp.src('css/*.css')
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('cssoutput'));
+});
+
+
 gulp.task('watch', function() {
+    gulp.watch('css/*.css', ['styles']);
     gulp.watch('js/*.js', ['es6']);
 });
